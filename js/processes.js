e88653c62b2068d9ecdf5e5a86289bbc461128a8
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2021-10-17 18:21:55 -0500

    Added processes data to interact

---
+++ js/processes.js
@@ -0,0 +1,36 @@
+var selectionProcesses = [
+  {
+    id: 1,
+    offerId: 1,
+    stages: [
+      {
+        id: 1,
+        name: "Primera entrevista",
+      },
+      {
+        id: 2,
+        name: "Recepción de documentos adicionales",
+      },
+      {
+        id: 3,
+        name: "Segunda entrevista",
+      },
+      {
+        id: 4,
+        name: "Solicitud de exámenes médicos",
+      },
+      {
+        id: 5,
+        name: "Resultados exámenes médicos",
+      },
+      {
+        id: 6,
+        name: "Prueba psicotécnica",
+      },
+      {
+        id: 7,
+        name: "Contratación",
+      },
+    ],
+  },
+];
